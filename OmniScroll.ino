5a153394bc009b36acc1bacc708b27a9b8101fa7
roufsweb <web.rouf@gmail.com>
2026-07-27 04:40:42 +0600

    Decrease touch sensitivity (increase threshold to 120)

--- OmniScroll.ino
+++ OmniScroll.ino
@@ -32,7 +32,7 @@ USBHIDMouse Mouse;
 USBHIDConsumerControl ConsumerControl;
 USBHIDKeyboard Keyboard;
 
-TouchController touch(TOUCH_PIN, 60); // Sensitivity matched to stat-screen project
+TouchController touch(TOUCH_PIN, 120); // Increased threshold to reduce accidental touches (less sensitive)
 
 enum Mode { MODE_SCROLL, MODE_VOLUME, MODE_TIMELINE };
 Mode currentMode = MODE_SCROLL;
